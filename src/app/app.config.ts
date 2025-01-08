10b21b94ab00336c736939575537be8915c58285
William Muniz <william.muniz@webpublico.com.br>
2025-01-08 15:59:50 -0300

    componente de datepicker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,8 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
@@ -12,6 +14,10 @@ export const appConfig: ApplicationConfig = {
   provideAnimationsAsync(),
   {
     provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' }
-  }
+  },
+  {
+    provide: MAT_DATE_LOCALE, useValue: 'pt-br'
+  },
+
   ]
 };
--- src/app/datepiker/datepiker.component.ts
+++ src/app/datepiker/datepiker.component.ts
@@ -1,15 +1,42 @@
 import { Component } from '@angular/core';
 import { MatNativeDateModule } from '@angular/material/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Platform } from '@angular/cdk/platform'
+import { MatMomentDateModule } from '@angular/material-moment-adapter'
+import moment from 'moment';
+
+import 'moment/locale/pt-br'; 
+
+moment.locale('pt-br'); 
+
 
 
 @Component({
   selector: 'app-datepiker',
-  imports: [MatDatepickerModule, MatFormFieldModule, MatNativeDateModule],
+  imports: [
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatMomentDateModule
+  ],
   templateUrl: './datepiker.component.html',
   styleUrl: './datepiker.component.scss'
 })
 export class DatepikerComponent {
+
+  startDate = moment([2024,0,1]);
+  minDate = moment([2023, 0, 1]);
+  maxDate = moment([2024, 11, 20]);
+
+  constructor(private platform: Platform) {
+
+  }
+
+  get isTouchDivice() {
+    return this.platform.ANDROID || this.platform.IOS;
+  }
 
 }
